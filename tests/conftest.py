3c80f36fb69183b7e44d58490e238974ed578c2e
SBT <sabir-bertit@hotmail.fr>
2025-05-29 19:34:03 +0200

    feat: add hero routes tests and improve test fixtures for better isolation
    Signed-off-by: SBT <sabir-bertit@hotmail.fr>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,44 +1,78 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlmodel import SQLModel, Session
+from starlette.testclient import TestClient
+
+from src.crud.hero import create_hero
+from src.crud.team import create_team
+from src.dependencies import get_session
+from src.main import app
 
 from src.models.hero import Hero
-from src.models.team import Team
+from src.models.team import Team, TeamCreate
 
 
-@pytest.fixture(name="session")
+@pytest.fixture(name="session", scope='module')
 def session_fixture():
+    test_db_url = "testing.db"
     # Create an in-memory SQLite database
     engine = create_engine(
-        "sqlite:///:memory:", connect_args={"check_same_thread": False}
+        f"sqlite:///{test_db_url}", connect_args={"check_same_thread": False}
     )
     SQLModel.metadata.create_all(engine)
     with Session(engine) as session:
-        # Heroes
-        hero_batman = Hero(name="Batman", secret_name="Bruce Wayne", age=35)
-        hero_superman = Hero(name="Superman", secret_name="Clark Kent", age=30)
-        hero_flash = Hero(name="Flash", secret_name="Barry Allen", age=28)
-        hero_cyborg = Hero(name="Cyborg", secret_name="Victor Stone", age=25)
-        # Teams
-        team_justice_league = Team(
-            name="Justice League",
-            headquarters="Gotham",
-            heroes=[hero_batman, hero_superman, hero_flash],
-        )
-        team_humanity = Team(
-            name="Humanity",
-            headquarters="Earth",
-        )
-        # Link heroes to teams:
-        team_justice_league.heroes = [hero_batman, hero_superman]
-        team_humanity.heroes = [hero_cyborg, hero_flash]
-        session.add(team_justice_league)
-        session.add(team_humanity)
-        session.commit()
-        session.refresh(team_justice_league)
-        session.refresh(team_humanity)
 
         yield session
 
         # Clean up the memory database after tests
         SQLModel.metadata.drop_all(engine)
+
+
+@pytest.fixture(name='client')
+def client_fixture(session):
+    # Heroes
+    hero_batman = Hero(name="Batman", secret_name="Bruce Wayne", age=35)
+    hero_superman = Hero(name="Superman", secret_name="Clark Kent", age=30)
+    hero_flash = Hero(name="Flash", secret_name="Barry Allen", age=28)
+    hero_cyborg = Hero(name="Cyborg", secret_name="Victor Stone", age=25)
+    # Teams
+    team_justice_league = Team(
+        name="Justice League",
+        headquarters="Gotham",
+        heroes=[hero_batman, hero_superman, hero_flash],
+    )
+    team_humanity = Team(
+        name="Humanity",
+        headquarters="Earth",
+    )
+    # Link heroes to teams:
+    team_justice_league.heroes = [hero_batman, hero_superman]
+    team_humanity.heroes = [hero_cyborg, hero_flash]
+    session.add(team_justice_league)
+    session.add(team_humanity)
+    session.commit()
+    session.refresh(team_justice_league)
+    session.refresh(team_humanity)
+
+    def override_get_session():
+        return session
+
+    app.dependency_overrides[get_session] = override_get_session
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture
+def batman_is_here(session):
+    hero_batman = Hero(name="Batman", secret_name="Bruce Wayne", age=35)
+    return create_hero(hero_batman, session)
+
+
+@pytest.fixture
+def team_avengers_is_here(session):
+    team = TeamCreate(
+        name="Avengers",
+        headquarters="LA",
+    )
+    return create_team(team, session)
---
+++ tests/routers/test_heroes.py
@@ -0,0 +1,118 @@
+def test_create_hero(client):
+    # Arrange
+    hero_data = {
+        'name': 'Deadpool',
+        'secret_name': 'Dive Wilson'
+    }
+
+    # Act
+    response = client.post(url='/heroes/', json=hero_data)
+    result = response.json()
+
+    # Assert
+    assert response.status_code == 201
+    assert result['name'] == hero_data['name']
+    assert result['secret_name'] == hero_data['secret_name']
+    assert result['age'] is None
+    assert result['id'] is not None
+    assert result['team_id'] is None
+    assert 'hashed_password' not in result
+
+
+def test_list_heroes(client):
+    # Arrange:
+    offset = 0
+    limit = 1
+    expected_data = [
+        {
+            'name': 'Batman',
+            'secret_name': 'Bruce Wayne',
+            'age': 35,
+            'team_id': 1,
+            'id': 1,
+            'team': {
+                'name': 'Justice League',
+                'headquarters': 'Gotham',
+                'id': 1
+            }
+        }
+    ]
+
+    # Act:
+    response = client.get('/heroes/', params={'offset': offset, 'limit': limit})
+    gotten_data = response.json()
+
+    # Assert
+    assert response.status_code == 200
+    assert gotten_data == expected_data
+
+
+def test_get_existing_hero_by_id(client):
+    # Arrange:
+    hero_id = 1
+
+    # Act:
+    response = client.get(f'/heroes/{hero_id}')
+
+    # Assert
+    assert response.status_code == 200
+
+
+def test_get_unexisting_hero_by_id(client):
+    # Arrange:
+    hero_id = 111111
+
+    # Act:
+    response = client.get(f'/heroes/{hero_id}')
+
+    # Assert
+    assert response.status_code == 404
+
+
+def test_update_existing_hero(client):
+    # Arrange
+    hero_id = 1
+    hero_data = {
+        "name": "Shazam", "secret_name": "Billy Batson", "age": 15
+    }
+
+    # Act
+    response = client.patch(url=f"/heroes/{hero_id}", json=hero_data)
+    data = response.json()
+
+    # Assert
+    assert response.status_code == 200
+    assert data['name'] == hero_data['name']
+
+
+def test_update_unexisting_hero(client):
+    # Arrange
+    hero_id = 0
+
+    # Act
+    response = client.patch(url=f"/heroes/{hero_id}", json={})
+
+    # Assert
+    assert response.status_code == 404
+
+
+def test_delete_existing_hero(client):
+    # Arrange
+    hero_id = 1
+
+    # Act
+    response = client.delete(url=f"/heroes/{hero_id}")
+
+    # Assert
+    assert response.status_code == 204
+
+
+def test_delete_unexisting_hero(client):
+    # Arrange
+    hero_id = 0
+
+    # Act
+    response = client.delete(url=f"/heroes/{hero_id}")
+
+    # Assert
+    assert response.status_code == 404
