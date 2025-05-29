e1a6b03e8835702dd347620e5e8a7e8c5c0ecaae
SBT <sabir-bertit@hotmail.fr>
2025-05-29 15:42:22 +0200

    feat: update heroes and teams endpoints to return nested relationship attributes.
    Signed-off-by: SBT <sabir-bertit@hotmail.fr>

--- src/routers/heroes.py
+++ src/routers/heroes.py
@@ -3,10 +3,11 @@ from typing import Annotated
 from fastapi import Query, HTTPException, APIRouter
 from starlette import status
 
-from src.crud.hero import create_hero, get_heroes, get_hero_by_id, HeroNotFoundError, update_hero as update_repo, \
-    delete_hero as delete_repo
+from src.crud.hero import create_hero, get_heroes, get_hero_by_id, HeroNotFoundError, update_hero, \
+    delete_hero
 from src.dependencies import SessionDep
-from src.models.hero import HeroPublic, HeroCreate, HeroUpdate
+from src.models.hero import HeroCreate, HeroUpdate, Hero
+from src.models.public import HeroPublicWithTeam, HeroPublic
 
 router = APIRouter(
     prefix='/heroes',
@@ -15,24 +16,24 @@ router = APIRouter(
 @router.post(
     path="/", status_code=status.HTTP_201_CREATED, response_model=HeroPublic
 )
-async def create_heros(hero: HeroCreate, session: SessionDep):
+async def create(hero: HeroCreate, session: SessionDep) -> Hero:
     return create_hero(hero=hero, session=session)
 
 
-@router.get(path="/", response_model=list[HeroPublic])
-async def read_heroes(
+@router.get(path="/", response_model=list[HeroPublicWithTeam])
+async def list_heroes(
     session: SessionDep,
     offset: Annotated[int, Query(ge=0)] = 0,
     limit: Annotated[int, Query(le=100)] = 100,
-):
+) -> list[Hero]:
     return get_heroes(session=session, offset=offset, limit=limit)
 
 
-@router.get(path="/{hero_id}", response_model=HeroPublic)
-async def read_hero(
+@router.get(path="/{hero_id}", response_model=HeroPublicWithTeam)
+async def get_hero(
     hero_id: int,
     session: SessionDep,
-):
+) -> Hero:
     try:
         return get_hero_by_id(hero_id=hero_id, session=session)
     except HeroNotFoundError as e:
@@ -40,16 +41,16 @@ async def read_hero(
 
 
 @router.patch(path="/{hero_id}", response_model=HeroPublic)
-async def update_hero(hero_id: int, hero: HeroUpdate, session: SessionDep):
+async def update(hero_id: int, hero: HeroUpdate, session: SessionDep) -> Hero:
     try:
-        return update_repo(hero_id=hero_id, hero=hero, session=session)
+        return update_hero(hero_id=hero_id, hero=hero, session=session)
     except HeroNotFoundError as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=e.message)
 
 
 @router.delete("/{hero_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_hero(hero_id: int, session: SessionDep):
+async def delete(hero_id: int, session: SessionDep):
     try:
-        delete_repo(hero_id=hero_id, session=session)
+        delete_hero(hero_id=hero_id, session=session)
     except HeroNotFoundError as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=e.message)
--- src/routers/teams.py
+++ src/routers/teams.py
@@ -4,9 +4,10 @@ from fastapi import Query, HTTPException, APIRouter
 from starlette import status
 
 from src.crud.team import create_team, get_teams, get_team_by_id, TeamNotFoundError, update_team as update_repo, \
-    delete_team as delete_repo
+    delete_team
 from src.dependencies import SessionDep
-from src.models.team import TeamPublic, TeamCreate, TeamUpdate
+from src.models.team import TeamCreate, TeamUpdate, Team
+from src.models.public import TeamPublicWithHeroes, TeamPublic
 
 router = APIRouter(
     prefix='/teams',
@@ -15,24 +16,24 @@ router = APIRouter(
 @router.post(
     path="/", status_code=status.HTTP_201_CREATED, response_model=TeamPublic
 )
-async def create(team: TeamCreate, session: SessionDep):
+async def create(team: TeamCreate, session: SessionDep) -> Team:
     return create_team(team=team, session=session)
 
 
-@router.get(path="/", response_model=list[TeamPublic])
+@router.get(path="/", response_model=list[TeamPublicWithHeroes])
 async def list_teams(
     session: SessionDep,
     offset: Annotated[int, Query(ge=0)] = 0,
     limit: Annotated[int, Query(le=100)] = 100,
-):
+) -> list[Team]:
     return get_teams(session=session, offset=offset, limit=limit)
 
 
-@router.get(path="/{team_id}", response_model=TeamPublic)
+@router.get(path="/{team_id}", response_model=TeamPublicWithHeroes)
 async def get_team(
     team_id: int,
     session: SessionDep,
-):
+) -> Team:
     try:
         return get_team_by_id(team_id=team_id, session=session)
     except TeamNotFoundError as e:
@@ -40,7 +41,7 @@ async def get_team(
 
 
 @router.patch(path="/{team_id}", response_model=TeamPublic)
-async def update(team_id: int, team: TeamUpdate, session: SessionDep):
+async def update(team_id: int, team: TeamUpdate, session: SessionDep) -> Team:
     try:
         return update_repo(team_id=team_id, team=team, session=session)
     except TeamNotFoundError as e:
@@ -50,6 +51,6 @@ async def update(team_id: int, team: TeamUpdate, session: SessionDep):
 @router.delete("/{team_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete(team_id: int, session: SessionDep):
     try:
-        delete_repo(team_id=team_id, session=session)
+        delete_team(team_id=team_id, session=session)
     except TeamNotFoundError as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
