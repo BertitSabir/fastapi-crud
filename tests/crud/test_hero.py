eea04583a49a5aed6d44913d3726462d9a982a1e
SBT <sabir-bertit@hotmail.fr>
2025-05-29 19:33:23 +0200

    refactor: improve test structure and consistency in hero tests
    Signed-off-by: SBT <sabir-bertit@hotmail.fr>

--- tests/crud/test_hero.py
+++ tests/crud/test_hero.py
@@ -1,5 +1,5 @@
 import pytest
-from src.models.hero import Hero, HeroCreate, HeroUpdate
+from src.models.hero import HeroCreate, HeroUpdate, Hero
 
 from src.crud.hero import (
     get_heroes,
@@ -7,15 +7,20 @@ from src.crud.hero import (
     create_hero,
     update_hero,
     delete_hero,
-    HeroNotFoundError, hash_password,
+    HeroNotFoundError,
+    hash_password,
 )
 
 
 def test_create_hero(session):
     # Arrange
-    password = 'password'
+    password = "password"
     hero = HeroCreate(
-        name="Spiderman 3", secret_name="Miles Morales", age=25, password=password, team_id=1
+        name="Spiderman 3",
+        secret_name="Miles Morales",
+        age=25,
+        password=password,
+        team_id=1,
     )
 
     # Act
@@ -31,20 +36,29 @@ def test_create_hero(session):
 
 def test_list(session):
     # Arrange
+    # Heroes
+    hero_batman = Hero(name="Batman", secret_name="Bruce Wayne", age=35)
+    hero_superman = Hero(name="Superman", secret_name="Clark Kent", age=30)
+    hero_flash = Hero(name="Flash", secret_name="Barry Allen", age=28)
+    hero_cyborg = Hero(name="Cyborg", secret_name="Victor Stone", age=25)
+    heroes = [hero_batman, hero_superman, hero_flash, hero_cyborg]
+    for hero in heroes:
+        create_hero(hero, session)
+
     # Act
     heroes = get_heroes(session=session, offset=0, limit=100)
 
     # Assert
-    assert len(heroes) == 4
+    assert len(heroes) == 5
     assert heroes[0].id == 1
     assert heroes[1].id == 2
     assert heroes[2].id == 3
     assert heroes[3].id == 4
 
 
-def test_get_existing_hero_by_id(session):
+def test_get_existing_hero_by_id(session, batman_is_here):
     # Arrange
-    hero_id = 1
+    hero_id = batman_is_here.id
 
     # Act
     hero = get_hero_by_id(hero_id=hero_id, session=session)
@@ -65,28 +79,28 @@ def test_get_unexisting_hero_by_id(session):
         get_hero_by_id(hero_id=hero_id, session=session)
 
 
-def test_update_hero(session):
+def test_update_hero(session, batman_is_here):
     # Arrange
-    new_password = 'newpassword'
+    new_password = "newpassword"
     hashed_new_password = hash_password(password=new_password)
-    original_hero = get_hero_by_id(hero_id=1, session=session)
+    hero_batman = batman_is_here
     updated_hero = HeroUpdate(name="Spiderman 3", age=31, password=new_password)
 
     # Act
     updated_hero = update_hero(
-        hero_id=original_hero.id, hero=updated_hero, session=session
+        hero_id=hero_batman.id, hero=updated_hero, session=session
     )
 
     # Assert
-    assert updated_hero.id == original_hero.id
+    assert updated_hero.id == hero_batman.id
     assert updated_hero.name == "Spiderman 3"
     assert updated_hero.age == 31
     assert updated_hero.hashed_password == hashed_new_password
 
 
-def test_delete_hero(session):
+def test_delete_hero(session, batman_is_here):
     # Arrange
-    hero_id = 1
+    hero_id = batman_is_here.id
 
     # Act
     deleted = delete_hero(hero_id=hero_id, session=session)
